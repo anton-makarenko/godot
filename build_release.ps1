f963b9f4681f843e4e573629611c9d8060468dce
Anton Makarenko <makarenkoanton2013@gmail.com>
2026-06-29 17:57:26 +0300

    Edits build script

--- build_release.ps1
+++ build_release.ps1
@@ -1,6 +1,6 @@
 . .\switch_branch.ps1
 Switch-Branch
 
-scons use_mingw=yes platform=windows target=editor module_mono_enabled=yes production=yes lto=full mesa_libs=C:\Users\makar\AppData\Local\Godot\build_deps\mesa-x86_64-llvm accesskit=no winrt=no angle=no
+scons use_mingw=yes platform=windows target=editor module_mono_enabled=yes lto=full mesa_libs=C:\Users\makar\AppData\Local\Godot\build_deps\mesa-x86_64-llvm accesskit=no winrt=no angle=no copy_mono_root=yes
 .\bin\godot.windows.editor.x86_64.llvm.mono.exe --headless --generate-mono-glue modules/mono/glue
 python modules/mono/build_scripts/build_assemblies.py --godot-output-dir=./bin
