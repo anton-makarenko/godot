7e63e88aa2d9f6218a7b93b4a2a307c15b5cadf5
Anton Makarenko <makarenkoanton2013@gmail.com>
2026-07-01 16:22:54 +0300

    Copies necessary dll

--- build_release.ps1
+++ build_release.ps1
@@ -1,5 +1,8 @@
 . .\switch_branch.ps1
+. .\copy_steam_api_dll.ps1
+
 Switch-Branch
+Copy-Steam-Api-Dll
 
 scons use_mingw=yes platform=windows target=editor module_mono_enabled=yes lto=full mesa_libs=C:\Users\makar\AppData\Local\Godot\build_deps\mesa-x86_64-llvm accesskit=no winrt=no angle=no copy_mono_root=yes
 .\bin\godot.windows.editor.x86_64.llvm.mono.exe --headless --generate-mono-glue modules/mono/glue
---
+++ copy_steam_api_dll.ps1
@@ -0,0 +1,4 @@
+function Copy-Steam-Api-Dll {
+	New-Item -ItemType Directory -Force -Path "bin"
+	Copy-Item "modules\godotsteam\sdk\redistributable_bin\win64\steam_api64.dll" -Destination "bin"
+}
