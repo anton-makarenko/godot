22ed6d5a61dcdbe8dc8b06fd35b511cb2ece8b8c
Anton Makarenko <makarenkoanton2013@gmail.com>
2026-07-01 15:27:13 +0300

    Updates godotsteam

--- build_release.ps1
+++ build_release.ps1
@@ -5,4 +5,4 @@ scons use_mingw=yes platform=windows target=editor module_mono_enabled=yes lto=f
 .\bin\godot.windows.editor.x86_64.llvm.mono.exe --headless --generate-mono-glue modules/mono/glue
 python modules/mono/build_scripts/build_assemblies.py --godot-output-dir=./bin
 
-scons use_mingw=yes platform=windows target=template_release arch=x86_64 lto=full mesa_libs=C:\Users\makar\AppData\Local\Godot\build_deps\mesa-x86_64-llvm accesskit=no winrt=no angle=no module_mono_enabled=yes copy_mono_root=yes
+#scons use_mingw=yes platform=windows target=template_release arch=x86_64 lto=full mesa_libs=C:\Users\makar\AppData\Local\Godot\build_deps\mesa-x86_64-llvm accesskit=no winrt=no angle=no module_mono_enabled=yes copy_mono_root=yes
