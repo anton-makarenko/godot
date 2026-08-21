d814dc2fa402db1339d4214f7c32b32b3c0a9098
Anton Makarenko <anton.makarenko.answer42@gmail.com>
2026-08-21 21:14:09 +0300

    Fixes switch branch script

--- switch_branch.sh
+++ switch_branch.sh
@@ -1,9 +1,11 @@
-git fetch upstream --tags
-release_tag = $(git describe --tags --abbrev=0)
-my_branch_name = "custom-build"
+#!/bin/bash
 
-if [ -z "$(git ls-remote origin $release_tag)" ]; then
-	git checkout -b $my_branch_name $release_tag
+git fetch upstream --tags
+release_tag=$(git describe --tags --abbrev=0)
+my_branch_name="custom-build"
+
+if [ -z "$(git ls-remote origin $my_branch_name)" ]; then
+    git checkout -b $my_branch_name $release_tag
 else
-	git switch $my_branch_name
+    git switch $my_branch_name
 fi
