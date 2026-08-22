4505a3aa8c95c05b6082d9472a473d8de798fbf1
Anton Makarenko <anton.makarenko.answer42@gmail.com>
2026-08-22 22:29:11 +0300

    Adding nuget source for the engine

--- build_engine.ps1
+++ build_engine.ps1
@@ -5,3 +5,4 @@
 scons use_mingw=yes platform=windows target=editor module_mono_enabled=yes production=yes lto=full mesa_libs=$HOME\AppData\Local\Godot\build_deps\mesa-x86_64-llvm accesskit=no winrt=no angle=no
 .\bin\godot.windows.editor.x86_64.llvm.mono.exe --headless --generate-mono-glue modules/mono/glue
 python modules/mono/build_scripts/build_assemblies.py --godot-output-dir=./bin
+dotnet nuget add source (Resolve-Path .\bin\GodotSharp\Tools\nupkgs).Path --name GodotNugetSource
--- build_engine.sh
+++ build_engine.sh
@@ -7,3 +7,4 @@
 scons platform=linuxbsd target=editor arch=x86_64 module_mono_enabled=yes production=yes lto=full accesskit=no
 ./bin/godot.linuxbsd.editor.x86_64.mono --headless --generate-mono-glue modules/mono/glue
 python modules/mono/build_scripts/build_assemblies.py --godot-output-dir=./bin
+dotnet nuget add source $(realpath ./bin/GodotSharp/Tools/nupkgs) --name GodotNugetSource
