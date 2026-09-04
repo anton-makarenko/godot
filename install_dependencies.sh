f9e1cd5c8461ed51d944b1f82a3238e9bd8dd38a
Anton Makarenko <anton.makarenko.answer42@gmail.com>
2026-09-05 01:16:07 +0300

    Dependencies for WSL

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -9,3 +9,6 @@ sudo dnf install -y \
   libstdc++-static \
   wayland-devel \
   dotnet-sdk-10.0
+  
+# For WSL
+sudo dnf install -y mesa-libGL mesa-libEGL mesa-libGLU mesa-dri-drivers fontconfig libX11 libXcursor libXinerama libXrandr libXi libdecor libxkbcommon xkeyboard-config
